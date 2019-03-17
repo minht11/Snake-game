899a7bb05649f2ccd2c970bb34d75eb3f4b77c14
minht11 <justas543@gmail.com>
2019-03-17 23:37:31 +0200

    Refactor input interface

---
+++ src/wasm/input/input.cpp
@@ -0,0 +1,29 @@
+#include <SDL2/SDL.h>
+#include "keyboard.cpp"
+#include "touch.cpp"
+
+class Input : protected Keyboard, protected Touch {
+  private:
+  SDL_Keycode direction = SDLK_RIGHT;
+
+  public:
+  void setEvent(SDL_Event event) {
+    SDL_Keycode tempDirection = -1;
+    if (event.type == SDL_KEYDOWN) {
+      tempDirection = findKeyDirection(event.key.keysym.sym);
+    }
+    if (event.type == SDL_FINGERDOWN) {
+      touchStart(event.tfinger);
+    }
+    if (event.type == SDL_FINGERMOTION) {
+      tempDirection = touchMove(event.tfinger);
+    }
+    if (tempDirection != -1) {
+      direction = tempDirection;
+    }
+  }
+
+  SDL_Keycode getDirection() {
+    return direction;
+  }
+};
--- src/wasm/main.cpp
+++ src/wasm/main.cpp
@@ -1,67 +1,17 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <iostream>
-#include <cmath>
-
 #include <emscripten/emscripten.h>
 
 #include "game.cpp"
+#include "input/input.cpp"
 
 static int quit = 0;
 
 Game game;
+Input input;
 
 double TIME_PER_TICK = 160.0;
 double lastTickTime;
 double lastFrameTime;
-
-void setGameDirection() {
-  SDL_Event event;
-  while (SDL_PollEvent(&event)) {
-    switch (event.type) {
-      case SDL_QUIT:
-        quit = 1;
-        break;
-    
-      case SDL_KEYDOWN:
-        switch (event.key.keysym.sym) {
-          case SDLK_w:
-          case SDLK_UP:
-            game.changeDirection(SDLK_UP);
-            break;
-          case SDLK_s:
-          case SDLK_DOWN:
-            game.changeDirection(SDLK_DOWN);
-            break;
-          case SDLK_a:
-          case SDLK_LEFT:
-            game.changeDirection(SDLK_LEFT);
-            break;
-          case SDLK_d:
-          case SDLK_RIGHT:
-            game.changeDirection(SDLK_RIGHT);
-            break;
-        }
-        return;
-
-      case SDL_FINGERMOTION:
-        auto dx = event.tfinger.dx;
-        auto dy = event.tfinger.dy;
-        if (std::fabs(dx) > std::fabs(dy)) {
-          game.changeDirection(dx > 0.00 ? SDLK_RIGHT : SDLK_LEFT);
-        } else {
-          game.changeDirection(dy > 0.00 ? SDLK_DOWN : SDLK_UP);
-        }
-        // EM_ASM_({
-        //   console.log($0, $1);
-        // }, event.tfinger.dx, event.tfinger.dy);
-          // EM_ASM_({
-          //   console.log("touch");
-          // });
-        break;
-    }
-  }
-}
 
 void main_tick() {
   if (!game.isInitialized) {
@@ -72,9 +22,13 @@ void main_tick() {
     emscripten_pause_main_loop();
     return;
   }
+  SDL_Event event;
+  while (SDL_PollEvent(&event)) {
+    input.setEvent(event);
+  }
 
   while (emscripten_get_now() > lastTickTime + TIME_PER_TICK) {
-    setGameDirection();
+    game.changeDirection(input.getDirection());
     game.logic();
     if (game.isGameOver) {
       game.playing = false;
